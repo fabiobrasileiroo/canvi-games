65b6627ff787d02170a30e354e014c45b58efe86
fabiobrasileiroo <fabio.h591@gmail.com>
2025-06-16 16:09:30 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -121,26 +121,29 @@ const GameRedirectPage: React.FC = () => {
             );
           })}
         </div>
-      <motion.div
-        className="flex justify-center items-center mt-6 w-full max-w-[800px] relative z-10"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.4 }}
-      >
-        <div className="rounded-lg flex flex-col items-center gap-2">
-          <h2 className="bg-white px-4 py-2 rounded-lg font-bold shadow-md text-lg">Patrocínio Oficial</h2>
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-col items-center gap-2 bg-white px-4 py-3 rounded-lg shadow-md">
-              <Image src="/assets/sponsorship/logo-zaplus.png" width={90} height={90} alt="Zaplus" />
-              <Image src="/assets/qr-zaplus-car.png" alt="QR Zaplus" width={100} height={100} />
+
+        {/* Card de Parceiros - Centralizado */}
+        <div className="flex justify-center items-center mt-8 w-full">
+          <motion.div
+            className="flex justify-center items-center max-w-[800px] relative z-10"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+          >
+            <div className="rounded-lg flex flex-col items-center gap-2">
+              <h2 className="bg-white px-4 py-2 rounded-lg font-bold shadow-md text-lg">Patrocínio Oficial</h2>
+              <div className="flex flex-col gap-2">
+                <div className="flex flex-col items-center gap-2 bg-white px-4 py-3 rounded-lg shadow-md">
+                  <Image src="/assets/sponsorship/logo-zaplus.png" width={90} height={90} alt="Zaplus" />
+                  <Image src="/assets/qr-zaplus-car.png" alt="QR Zaplus" width={100} height={100} />
+                </div>
+                <p className="bg-white px-2 py-1 rounded-lg text-center shadow-md text-sm">Siga a gente no instagram</p>
+              </div>
             </div>
-            <p className="bg-white px-2 py-1 rounded-lg text-center shadow-md text-sm">Siga a gente no instagram</p>
-          </div>
+          </motion.div>
         </div>
-      </motion.div>
       </div>
-    {/* </motion.div> */}
-    </div >
+    </div>
   );
 };
 
